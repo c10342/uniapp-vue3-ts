c207ee9e27e6ff6383d70153757a929bd4e28d4c
c10342 <1376023740@qq.com>
2024-06-13 23:02:27 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <view class="content">
+    <image class="logo" src="/static/logo.png" />
+    <view class="text-area">
+      <text class="title">{{ title }}</text>
+    </view>
+    <uni-card title="基础卡片" sub-title="副标题" extra="额外信息"
+      thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png">
+      <text>这是一个带头像和双标题的基础卡片，此示例展示了一个完整的卡片。</text>
+    </uni-card>
+  </view>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const title = ref('Hello123123')
+</script>
+
+<style>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.logo {
+  height: 200rpx;
+  width: 200rpx;
+  margin-top: 200rpx;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 50rpx;
+}
+
+.text-area {
+  display: flex;
+  justify-content: center;
+}
+
+.title {
+  font-size: 36rpx;
+  color: #8f8f94;
+}
+</style>
